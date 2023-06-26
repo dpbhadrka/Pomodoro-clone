617c380f37b80c94f134c7882c396f4d3cd839aa
Divyesh Bhadrka <p.bhadrka7@gecg28.ac.in>
2023-06-27 03:19:46 +0530

    Implement responsive design.

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -10,12 +10,14 @@ export default function Header() {
         <div className="projectName">
           <img className="logo" src={PomodoroLogo} alt="logo" />
           <div id="projectTitle">
-            Pomodoro<span id="sortname">Clone</span>
+            Pomodoro<span>Clone</span>
           </div>
         </div>
         <div className="about">
-          <a href="#information">About</a>
-          <a id="aboutIcon" href="./aboutME" target="_blank">
+          <a id="aboutLink" href="#information">
+            About
+          </a>
+          <a id="aboutIcon" href="#information">
             <img src={AboutIcon} alt="About" />
           </a>
         </div>
